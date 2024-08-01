7b131c85f0e401ec6eef67abd79d7f1c1d94a960
daracoo <dkuzmanoski21@gmail.com>
2024-08-01 16:23:11 +0200

    When clicking save projects list on the projects sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -26,12 +26,11 @@ function App() {
       }
       return {
         ...prevState,
+        selectedProject: undefined,
         projects: [...prevState.projects, newProject]
       }
     })
   }
-
-  console.log(projectsState);
 
   let content;
 
@@ -43,7 +42,7 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject}/>
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
       {content}
     </main>
   );
