5efed48e756d00942dc8ebbba304cd2200769acd
daracoo <dkuzmanoski21@gmail.com>
2024-08-01 12:21:42 +0200

    New Project component with inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NewProject from "./assets/components/NewProject";
 import ProjectsSidebar from "./assets/components/ProjectSidebar";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
+      <NewProject />
     </main>
   );
 }
