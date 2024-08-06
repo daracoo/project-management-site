7a467c27687688611c097df503b80f556f3b209f
daracoo <dkuzmanoski21@gmail.com>
2024-08-06 11:47:27 +0200

    Made projects selectable & viewing project details

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectSidebar";
+import SelectedProject from "./components/SelectedProject"
 import { useState} from "react";
 
 function App() {
@@ -27,6 +28,15 @@ function App() {
     });
   }
 
+  function handleSelectProject(id){
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  }
+
   function handleAddProject(projectData){
     setProjectsState(prevState => {
       const newProject = {
@@ -41,7 +51,9 @@ function App() {
     })
   }
 
-  let content;
+  const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
+
+  let content = <SelectedProject project={selectedProject}/>;
 
   if(projectsState.selectedProjectId === null){
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
@@ -51,7 +63,9 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} 
+      projects={projectsState.projects}
+      onSelectProject={handleSelectProject}/>
       {content}
     </main>
   );
