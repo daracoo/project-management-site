6fe29f69d2e45fa544d99352b6debc521b261500
daracoo <dkuzmanoski21@gmail.com>
2024-08-06 11:53:37 +0200

    Delete Project button functionality

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,4 +1,4 @@
-export default function selectedProject({project}){
+export default function selectedProject({project, onDelete}){
     
     const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
         year: 'numeric',
@@ -10,7 +10,7 @@ export default function selectedProject({project}){
         <header className="pb-4 mb-4 border-b-2 border-stone-300">
             <div className="flex items-center justify-between">
                 <h1 className="text-3xl font-bold text-stone-600">{project.title}</h1>
-                <button className="text-stone-600 hover:text-stone-950">delete</button>    
+                <button onClick={onDelete} className="text-stone-600 hover:text-stone-950">Delete</button>    
             </div>
             <p className="mb-4 text-stone-400">{formattedDate}</p>
             <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
